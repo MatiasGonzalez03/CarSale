e5f7f9e801a1f6d7a475dd7e3a4245c6d2f7e007
santibusnelli <santibusnelli57@gmail.com>
2021-12-01 18:17:55 -0300

    nuevo jsonautos y cartas en productos

--- Productos/script.js
+++ Productos/script.js
@@ -1,11 +1,33 @@
-const url = 'autos.json';
+let url = 'auto.json';
 const request = new XMLHttpRequest();
 request.open('GET', url);
 request.responseType = 'json';
 request.send();
-var autos = '';
+var auto = '';
 
 request.onload = function() {
+    auto = request.response;
     console.log(request.response);
-    autos = request.response;
+    setCartasAutos();
+}
+
+
+function setCartasAutos() {
+    let catalogo = auto.catalogo;
+    catalogo.forEach(element => {
+        let elementGalery = '<div class="col-md-6 col-lg-4"><div class="card" ' + element.id + '">';
+        elementGalery += '<img src= ' + element.img + ' class="card-img-top" alt="..."></img>';
+        elementGalery += '<div class="card-body">';
+        elementGalery += '<h5 class="card-title" style="font-size: 1.25rem;"> ' + element.nombre + ' </h5>';
+        elementGalery += '<p class="card-text"><b> Modelo: </b>' + element.modelo + ' </p>';
+        elementGalery += '<p class="card-text"><b> Kilometros: </b>' + element.kilometros + ' </p>';
+        elementGalery += '<p class="card-text"><b> Precio: </b>' + element.precio + ' </p>';
+        elementGalery += '<a href="#" class="btn btn-primary d-flex justify-content-center">Ver detalles</a>';
+        elementGalery += '<div class="d-flex justify-content-between align-items-center">';
+        elementGalery += '<div class="btn-group">';
+        elementGalery += '</div></div></div></div></div>';
+
+        $('#gallery_view').append(elementGalery);
+
+    });
 }
