390a15c02de58cf36e9f3d5cfddd715bd6d00fe8
santibusnelli <santibusnelli57@gmail.com>
2021-12-04 04:48:04 -0300

    categorias dinamicas y nuevo json con modificaciones, el anterior se dejo de andar y cree uno nuevo

--- PaginaPrincipal/script.js
+++ PaginaPrincipal/script.js
@@ -1,4 +1,4 @@
-let url = '../auto.json';
+let url = '../autos.json';
 const request = new XMLHttpRequest();
 request.open('GET', url);
 request.responseType = 'json';
@@ -14,9 +14,10 @@ request.onload = function() {
 function AutosDestacados() {
     let catalogo = auto.catalogo;
     var contadorTrue = 1
+
     catalogo.forEach(element => {
         if (contadorTrue < 7) {
-            if (element.destacado == true) {
+            if (element.destacado == true && element.active == true) {
                 let elementGalery = '<div class="col-md-6 col-lg-4" "' + element.marca + '"><div class="card" ' + element.id + '">';
                 elementGalery += '<img src= ' + element.img + ' class="card-img-top" alt="..."></img>';
                 elementGalery += '<div class="card-body">';
@@ -35,3 +36,32 @@ function AutosDestacados() {
         }
     });
 }
+
+
+/*
+function AutosDestacadosAleatorio(){
+   let tamañoDelCatalogo = Object.keys(auto.catalogo).length
+   var ramdom = Math.floor(Math.random() * tamañoDelCatalogo)
+   console.log(ramdom)
+   for (var n = 0; n <= tamañoDelCatalogo; n++) {
+       var ramdom = Math.floor(Math.random() * tamañoDelCatalogo);
+       if (auto.catalogo.destacado[ramdom] == true && auto.catalogo.active[ramdom] == true) {
+           let elementGalery = '<div class="col-md-6 col-lg-4" "' + auto.catalogo.marca[ramdom] + '"><div class="card" ' + auto.catalogo.id[ramdom] + '">';
+           elementGalery += '<img src= ' + auto.catalogo.img[ramdom] + ' class="card-img-top" alt="..."></img>';
+           elementGalery += '<div class="card-body">';
+           elementGalery += '<h5 class="card-title" style="font-size: 1.25rem;"> ' + auto.catalogo.nombre[ramdom] + ' </h5>';
+           elementGalery += '<p class="card-text"><b> Modelo: </b>' + auto.catalogo.modelo[ramdom] + ' </p>';
+           elementGalery += '<p class="card-text"><b> Kilometros: </b>' + auto.catalogo.kilometros[ramdom] + ' </p>';
+           elementGalery += '<p class="card-text"><b> Precio: </b>' + auto.catalogo.precio[ramdom] + ' </p>';
+           elementGalery += '<a href="#" class="btn btn-primary d-flex justify-content-center">Ver detalles</a>';
+           elementGalery += '<div class="d-flex justify-content-between align-items-center">';
+           elementGalery += '<div class="btn-group">';
+           elementGalery += '</div></div></div></div></div>';
+
+           $('#gallery_view').append(elementGalery);
+           //contadorTrue = contadorTrue + 1;
+       }
+   }
+   console.log(auto.catalogo[ramdom])
+   }
+   */
--- Productos/script.js
+++ Productos/script.js
@@ -1,4 +1,4 @@
-let url = '../auto.json';
+let url = '../autos.json';
 const request = new XMLHttpRequest();
 request.open('GET', url);
 request.responseType = 'json';
@@ -9,48 +9,21 @@ request.onload = function() {
     auto = request.response;
     console.log(request.response);
     setCartasAutos();
-    //AutosDestacados();
 }
 
 
 function setCartasAutos() {
     let catalogo = auto.catalogo;
-    catalogo.forEach(element => {
-        let elementGalery = '<div class="col-md-6 col-lg-4 ' + element.marca + ' ' + element.modernidad + ' ' + element.marca + element.modernidad + '"><div class="card" ' + element.id + '">';
-        elementGalery += '<img src= ' + element.img + ' class="card-img-top" alt="..."></img>';
-        elementGalery += '<div class="card-body">';
-        elementGalery += '<h5 class="card-title" style="font-size: 1.25rem;"> ' + element.nombre + ' </h5>';
-        elementGalery += '<p class="card-text"><b> Modelo: </b>' + element.modelo + ' </p>';
-        elementGalery += '<p class="card-text"><b> Kilometros: </b>' + element.kilometros + ' </p>';
-        elementGalery += '<p class="card-text"><b> Precio: </b>' + element.precio + ' </p>';
-        elementGalery += '<a href="#" class="btn btn-primary d-flex justify-content-center">Ver detalles</a>';
-        elementGalery += '<div class="d-flex justify-content-between align-items-center">';
-        elementGalery += '<div class="btn-group">';
-        elementGalery += '</div></div></div></div></div>';
+    mostrarDestacados(catalogo);
+    mostrarNoDestacados(catalogo)
 
-        $('#gallery_view').append(elementGalery);
-
-    });
 }
 
-
-function Categorias() {
-    let catalogo = auto.catalogo;
+function mostrarDestacados(catalogo) {
     catalogo.forEach(element => {
-        if (element.modelo > 2000) {
-
-        }
-    })
-}
-
-/*
-function AutosDestacados() {
-    let catalogo = auto.catalogo;
-    var contadorTrue = 1
-    catalogo.forEach(element => {
-        if (contadorTrue < 7) {
-            if (element.destacado == true) {
-                let elementGalery = '<div class="col-md-6 col-lg-4" "' + element.marca + '"><div class="card" ' + element.id + '">';
+        if (element.destacado == true) {
+            if (element.active == true) {
+                let elementGalery = '<div class="col-md-6 col-lg-4 ' + element.marca + ' ' + element.modernidad + ' ' + element.marca + element.modernidad + '"><div class="card" ' + element.id + '">';
                 elementGalery += '<img src= ' + element.img + ' class="card-img-top" alt="..."></img>';
                 elementGalery += '<div class="card-body">';
                 elementGalery += '<h5 class="card-title" style="font-size: 1.25rem;"> ' + element.nombre + ' </h5>';
@@ -63,12 +36,32 @@ function AutosDestacados() {
                 elementGalery += '</div></div></div></div></div>';
 
                 $('#gallery_view').append(elementGalery);
-                contadorTrue = contadorTrue + 1;
             }
         }
     });
 }
-*/
+
+function mostrarNoDestacados(catalogo) {
+    catalogo.forEach(element => {
+        if (element.destacado == false) {
+            if (element.active == true) {
+                let elementGalery = '<div class="col-md-6 col-lg-4 ' + element.marca + ' ' + element.modernidad + ' ' + element.marca + element.modernidad + '"><div class="card" ' + element.id + '">';
+                elementGalery += '<img src= ' + element.img + ' class="card-img-top" alt="..."></img>';
+                elementGalery += '<div class="card-body">';
+                elementGalery += '<h5 class="card-title" style="font-size: 1.25rem;"> ' + element.nombre + ' </h5>';
+                elementGalery += '<p class="card-text"><b> Modelo: </b>' + element.modelo + ' </p>';
+                elementGalery += '<p class="card-text"><b> Kilometros: </b>' + element.kilometros + ' </p>';
+                elementGalery += '<p class="card-text"><b> Precio: </b>' + element.precio + ' </p>';
+                elementGalery += '<a href="#" class="btn btn-primary d-flex justify-content-center">Ver detalles</a>';
+                elementGalery += '<div class="d-flex justify-content-between align-items-center">';
+                elementGalery += '<div class="btn-group">';
+                elementGalery += '</div></div></div></div></div>';
+
+                $('#gallery_view').append(elementGalery);
+            }
+        }
+    });
+}
 
 $(document).ready(function() {
     $(".btnFiat").click(() => {
@@ -97,6 +90,17 @@ $(document).ready(function() {
     $(".fiatAntiguo").click(() => {
         $(".Fiatantiguo").show();
         $(".Peugeot").hide();
+        $(".moderno").hide();
+    })
+
+    $(".peugeotModerno").click(() => {
+        $(".Peugeotmoderno").show();
+        $(".Fiat").hide();
+        $(".antiguo").hide();
+    })
+    $(".peugeotAntiguo").click(() => {
+        $(".Peugeotantiguo").show();
+        $(".Fiat").hide();
         $(".moderno").hide();
     })
 
