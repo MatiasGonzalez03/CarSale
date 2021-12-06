04ece0b2017061b7e71a30cf58b8c9f17682fd8c
MatiasGonzalez03 <matiasgonzalezezequiel92@gmail.com>
2021-12-06 00:17:28 -0300

    Validacion de los datos en el formulario de contacto

---
+++ Contacto/validacion.js
@@ -0,0 +1,75 @@
+const form = document.getElementById('form');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const tel = document.getElementById('tel');
+const mensaje = document.getElementById('mensaje');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	const nombreValue = nombre.value.trim();
+	const emailValue = email.value.trim();
+	const telValue = tel.value.trim();
+	const mensajeValue = mensaje.value.trim();
+	
+	if(nombreValue === '') {
+		setErrorFor(nombre, 'Complete este campo');
+	} else if (!isNombre(nombreValue)) {
+		setErrorFor(nombre, 'Ingrese un nombre valido');
+	} else {
+		setSuccessFor(nombre);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Complete este campo');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Ingrese un mail valido');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(telValue === '') {
+		setErrorFor(tel, 'Complete este campo');
+	} else if (!isTel(telValue)) {
+		setErrorFor(tel, 'Ingrese un telefono valido');
+	} else {
+		setSuccessFor(tel);
+	}
+	
+	if(mensajeValue === '') {
+		setErrorFor(mensaje, 'Complete este campo');
+	} else if (document.getElementById("mensaje").value.length <= 40) {
+		setErrorFor(mensaje, 'No ha cumplido con el minimo de caracteres requeridos');
+	} else if (document.getElementById("mensaje").value.length >= 240) {
+		setErrorFor(mensaje, 'Ha sobrepasado el limite de caracteres permitidos');
+	} else {
+		setSuccessFor(mensaje);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email);
+}
+function isNombre(nombre) {
+	return /^[a-zA-ZÀ-ÿ\s]+\ [a-zA-ZÀ-ÿ\s]{2,40}$/.test(nombre);
+}
+function isTel(tel) {
+	return /^\d{7,14}$/.test(tel);
+}
+
