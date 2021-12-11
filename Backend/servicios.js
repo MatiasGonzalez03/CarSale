aaafef9544061c543ff9db0e852709265f33d8bb
santibusnelli <santibusnelli57@gmail.com>
2021-12-11 16:52:08 -0300

    botones para json

--- Backend/servicios.js
+++ Backend/servicios.js
@@ -13,8 +13,18 @@ request.onload = function() {
 
 $(document).ready(function() {
     $(".servicios").click(() => {
+        $(".BotonMostrarServicios").show();
+        $(".BotonocultarServicios").show();
+    })
+
+    $(".BotonMostrarServicios").click(() => {
         $(".mostrarServicios").show();
     })
+
+    $(".BotonocultarServicios").click(() => {
+        $(".mostrarServicios").hide();
+    })
+
 })
 
 function mostrarJson() {
@@ -25,17 +35,17 @@ function mostrarJson() {
     mostrarServicios += '<div><p>"active": ' + auto.active + '</p></div>';
     mostrarServicios += '<div><p>"catalogo":</p></div>';
     mostrarServicios += '<div><p>{</p></div>';
-    mostrarServicios += '<div><p>"id": ' + auto.catalogo[1].id + '</p></div>';
-    mostrarServicios += '<div><p>"marca": ' + auto.catalogo[1].marca + '</p></div>';
-    mostrarServicios += '<div><p>"nombre": ' + auto.catalogo[1].nombre + '</p></div>';
-    mostrarServicios += '<div><p>"kilometros": ' + auto.catalogo[1].kilometros + '</p></div>';
-    mostrarServicios += '<div><p>"precio": ' + auto.catalogo[1].precio + '</p></div>';
-    mostrarServicios += '<div><p>"modelo": ' + auto.catalogo[1].modelo + '</p></div>';
-    mostrarServicios += '<div><p>"img": ' + auto.catalogo[1].img + '</p></div>';
-    mostrarServicios += '<div><p>"descripcion": ' + auto.catalogo[1].descripcion + '</p></div>';
-    mostrarServicios += '<div><p>"destacado": ' + auto.catalogo[1].destacado + '</p></div>';
-    mostrarServicios += '<div><p>"modernidad": ' + auto.catalogo[1].modernidad + '</p></div>';
-    mostrarServicios += '<div><p>"active": ' + auto.catalogo[1].active + '</p></div>';
+    mostrarServicios += '<div><p>   "id": ' + auto.catalogo[1].id + '</p></div>';
+    mostrarServicios += '<div><p>   "marca": ' + auto.catalogo[1].marca + '</p></div>';
+    mostrarServicios += '<div><p>   "nombre": ' + auto.catalogo[1].nombre + '</p></div>';
+    mostrarServicios += '<div><p>   "kilometros": ' + auto.catalogo[1].kilometros + '</p></div>';
+    mostrarServicios += '<div><p>   "precio": ' + auto.catalogo[1].precio + '</p></div>';
+    mostrarServicios += '<div><p>   "modelo": ' + auto.catalogo[1].modelo + '</p></div>';
+    mostrarServicios += '<div><p>   "img": ' + auto.catalogo[1].img + '</p></div>';
+    mostrarServicios += '<div><p>   "descripcion": ' + auto.catalogo[1].descripcion + '</p></div>';
+    mostrarServicios += '<div><p>   "destacado": ' + auto.catalogo[1].destacado + '</p></div>';
+    mostrarServicios += '<div><p>   "modernidad": ' + auto.catalogo[1].modernidad + '</p></div>';
+    mostrarServicios += '<div><p>   "active": ' + auto.catalogo[1].active + '</p></div>';
     mostrarServicios += '<div><p>}</p></div>';
     $('.mostrarServicios').append(mostrarServicios);
 }
