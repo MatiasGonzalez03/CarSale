eb87045b7acc0da28ddc5b9eec2d433f013ef574
santibusnelli <santibusnelli57@gmail.com>
2021-12-13 01:57:25 -0300

    botones

--- Productos/script.js
+++ Productos/script.js
@@ -8,31 +8,22 @@ var auto = '';
 request.onload = function() {
     auto = request.response;
     console.log(request.response);
-    //setCartasAutos();
     var catalogo = auto.catalogo;
+
     cartas(catalogo);
 }
 
-
-function setCartasAutos() {
-    //let catalogo = auto.catalogo;
-    //mostrarDestacados(catalogo);
-    //mostrarNoDestacados(catalogo)
-    //cartas()
-
-}
-
-function cartas(catalogo) {
+function cartas(catalogo, orden) {
     catalogo.forEach(element => {
         if (element.active == true) {
-            let elementGalery = '<div class="col-md-6 col-lg-4 ' + element.marca + ' ' + element.modernidad + ' ' + element.marca + element.modernidad + '"><div class="card" ' + element.id + '">';
+            let elementGalery = '<div class="col-sm-12 col-md-6 col-lg-4 ' + orden + ' ' + element.marca + ' ' + element.modernidad + ' ' + element.marca + element.modernidad + '"><div class="card" ' + element.id + '">';
             elementGalery += '<img src= ' + element.img + ' class="card-img-top" alt="..."></img>';
             elementGalery += '<div class="card-body">';
             elementGalery += '<h5 class="card-title" style="font-size: 1.25rem;"> ' + element.nombre + ' </h5>';
             elementGalery += '<p class="card-text"><b> Modelo: </b>' + element.modelo + ' </p>';
             elementGalery += '<p class="card-text"><b> Kilometros: </b>' + element.kilometros + ' </p>';
             elementGalery += '<p class="card-text"><b> Precio: </b>' + element.precio + ' </p>';
-            elementGalery += '<a href="#" class="btn btn-primary d-flex justify-content-center">Ver detalles</a>';
+            elementGalery += '<a href="" class="btn btn-primary d-flex justify-content-center">Ver detalles</a>';
             elementGalery += '<div class="d-flex justify-content-between align-items-center">';
             elementGalery += '<div class="btn-group">';
             elementGalery += '</div></div></div></div></div>';
@@ -41,23 +32,7 @@ function cartas(catalogo) {
         }
     });
 }
-/*
-function mostrarDestacados(catalogo) {
-    catalogo.forEach(element => {
-        if (element.destacado == true) {
-                cartas(element);
-        }
-    });
-}
 
-function mostrarNoDestacados(catalogo) {
-    catalogo.forEach(element => {
-        if (element.destacado == false) {
-                cartas(element);
-        }
-    });
-}
-*/
 $(document).ready(function() {
     $(".btnFiat").click(() => {
         $(".Peugeot").hide();
@@ -99,20 +74,31 @@ $(document).ready(function() {
     $(".ordenarAsc").click(() => {
         var JsonAsc = sortJSON(auto.catalogo, 'nombre', 'asc');
         console.log(JsonAsc);
-        cartas(JsonAsc);
+        cartas(JsonAsc, "asc");
+        $(".asc").show();
+        $(".desc").hide();
+        $(".undefined").hide();
+        $(".destacadoss").hide();
     })
     $(".ordenarDesc").click(() => {
         var JsonDesc = sortJSON(auto.catalogo, 'nombre', 'desc');
         console.log(JsonDesc);
-        cartas(JsonDesc);
+        cartas(JsonDesc, "desc");
+        $(".desc").show();
+        $(".asc").hide();
+        $(".undefined").hide();
+        $(".destacadoss").hide();
     })
 
     $(".destacados").click(() => {
-        var JsonDesc = sortJSON(auto.catalogo, 'nombre', 'desc');
-        console.log(JsonDesc);
-        cartas(JsonDesc);
+        var JsonDestacado = sortJSON(auto.catalogo, 'destacado', 'desc');
+        console.log(JsonDestacado);
+        cartas(JsonDestacado, "destacadoss");
+        $(".destacadoss").show();
+        $(".desc").hide();
+        $(".asc").hide();
+        $(".undefined ").hide()
     })
-
 })
 
 function sortJSON(data, key, orden) {
@@ -130,3 +116,29 @@ function sortJSON(data, key, orden) {
     });
 
 }
+
+/*
+catalogo.forEach(element => {
+    if (element.marca == "Peugeot") {
+
+    }
+});
+
+for (var i = 0; i < catalogo.length; i++) {
+    if (catalogo[i].marca == "Peugeot") {
+        catalogo[i].splice(i, 1)
+    }
+}
+
+catalogo.forEach(function(catalogo, index, object) {
+    if (catalogo.marca === 'Peugeot') {
+        object.splice(index, 1);
+    }
+});
+*/
+/*
+auto.catalogo = auto.catalogo.filter(function(catalogo) {
+    return catalogo.marca == 'Fiat';
+})
+cartas(auto.catalogo)
+*/
